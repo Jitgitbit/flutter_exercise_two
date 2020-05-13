3809cecd0adc4a59a449f91cd45e798472705d12
Jitgitbit <rowowolf@gmail.com>
2020-05-13 23:13:06 +0200

    Not a Widget: Adding a Transaction Model & Transaction Data

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_exercise_two/transaction.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -13,6 +15,11 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(id: 't1', title: 'new shoes', amount: 69.99, date: DateTime.now(),),
+    Transaction(id: 't2', title: 'new hat', amount: 39.99, date: DateTime.now(),),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
