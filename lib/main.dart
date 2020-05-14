9ed304f9d0a68ae7995b9e36ccab3b91434fc3e3
Jitgitbit <rowowolf@gmail.com>
2020-05-14 16:57:02 +0200

    back to global Quicksand, prettier

--- lib/main.dart
+++ lib/main.dart
@@ -16,22 +16,22 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.purple,
         accentColor: Colors.amber,
         fontFamily: 'Quicksand',
-        textTheme: ThemeData.light().textTheme.copyWith(         
-          title: TextStyle(
-            fontFamily: 'OpenSans',
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        appBarTheme: AppBarTheme(
-          textTheme: ThemeData.light().textTheme.copyWith(
-            title: TextStyle(
-              fontFamily: 'OpenSans',
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
+        // textTheme: ThemeData.light().textTheme.copyWith(         
+        //   title: TextStyle(
+        //     fontFamily: 'OpenSans',
+        //     fontSize: 18,
+        //     fontWeight: FontWeight.bold,
+        //   ),
+        // ),
+        // appBarTheme: AppBarTheme(
+        //   textTheme: ThemeData.light().textTheme.copyWith(
+        //     title: TextStyle(
+        //       fontFamily: 'OpenSans',
+        //       fontSize: 20,
+        //       fontWeight: FontWeight.bold,
+        //     ),
+        //   ),
+        // ),
       ),
       home: MyHomePage(),
     );
