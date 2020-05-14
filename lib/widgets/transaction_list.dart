f353097d695b54ab22140be7d4ada822a4866c8c
Jitgitbit <rowowolf@gmail.com>
2020-05-14 23:50:50 +0200

    ! Here comes the ListTile widget !!!!!!!!!

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -32,44 +32,7 @@ class TransactionList extends StatelessWidget {
             )                                            // (ctx refers to context, which is meta-data that informs about relative position) !
           : ListView.builder(                           // Thanks to .builder() it only loads what is visible, instead of the entire list !!!!!
               itemBuilder: (ctx, index) {              // So obviously with long lists, this saves a lot of data being transferred !!!!!
-                return Card(                       
-                  child: Row(                     
-                    children: <Widget>[
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                          color: Theme.of(context).primaryColorDark,
-                          width: 2,
-                        )),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          '\$${transactions[index].amount.toStringAsFixed(2)}',              // two decimals, always
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Theme.of(context).primaryColor,
-                          ),
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            transactions[index].title,
-                            // style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                            style: Theme.of(context).textTheme.title,
-                          ),
-                          Text(
-                            DateFormat.yMMMd().format(transactions[index].date),
-                            style: TextStyle(color: Colors.grey),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                );
+                return ListTile();
               },
               itemCount: transactions.length,
             ),
