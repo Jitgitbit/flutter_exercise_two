316ff8fd74054f005fa3d1154c63466d31c5494b
Jitgitbit <rowowolf@gmail.com>
2020-05-14 20:41:02 +0200

    Good old for - if combo loop

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import '../models/transaction.dart';
 
@@ -9,8 +10,22 @@ class Chart extends StatelessWidget {
   Chart(this.recentTransactions);
 
   List<Map<String, Object>> get groupedTransactionValues {
-    List.generate(7, (index) {
-      return {'day': 'T', 'amount': 9.99};
+
+    return List.generate(7, (index) {
+
+      final weekDay = DateTime.now().subtract(Duration(days: index),);
+      var totalSum = 0.0;
+
+      for(var i = 0; i < recentTransactions.length; i++) {
+        if(
+          recentTransactions[i].date.day == weekDay.day &&
+          recentTransactions[i].date.month == weekDay.month &&
+          recentTransactions[i].date.year == weekDay.year
+        ){
+          totalSum += recentTransactions[i].amount;
+        }
+      }
+      return {'day': DateFormat.E(weekDay), 'amount': totalSum};
     });
   }
 
