71f57cf4e321a86d710fa092bacc352b176964da
Jitgitbit <rowowolf@gmail.com>
2020-05-14 13:34:27 +0200

    Working with ListViews

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -3,53 +3,57 @@ import 'package:intl/intl.dart';
 
 import '../models/transaction.dart';
 
-
 class TransactionList extends StatelessWidget {
-
   final List<Transaction> transactions;
 
-  TransactionList(this.transactions);                                // The Constructor ! transfering data from the parentWidget into this childWidget !!!
+  TransactionList(
+      this.transactions); // The Constructor ! transfering data from the parentWidget into this childWidget !!!
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((transaction) {                   // <------ Watch out with arrow fn in DART !!!   
-        return Card(
-          child: Row(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                decoration: BoxDecoration(
-                    border: Border.all(
-                  color: Colors.purple,
-                  width: 2,
-                )),
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  '\$${transaction.amount}',
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.purple),
+    return Container(
+      height: 300,
+      child: ListView.builder(                              
+        itemBuilder: (context, index) {
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                    color: Colors.purple,
+                    width: 2,
+                  )),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    '\$${transactions[index].amount}',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.purple),
+                  ),
                 ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    transaction.title,
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    DateFormat.yMMMd().format(transaction.date),
-                    style: TextStyle(color: Colors.grey),
-                  ),
-                ],
-              )
-            ],
-          ),
-        );
-      }).toList(),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      transactions[index].title,
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      DateFormat.yMMMd().format(transactions[index].date),
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          );
+        },
+        itemCount: transactions.length,
+      ),
     );
   }
 }
