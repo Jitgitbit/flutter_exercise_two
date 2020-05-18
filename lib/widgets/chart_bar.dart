6d2d1f71a6159c29dbd67e8638a16b102751744e
Jitgitbit <rowowolf@gmail.com>
2020-05-18 16:33:10 +0200

    Using the LayoutBuilder Widget

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class ChartBar extends StatelessWidget {
-
   final String label;
   final double spendingAmount;
   final double spendingPctOfTotal;
@@ -10,45 +9,56 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(builder: (ctx, constraint){},); Column(
-      children: <Widget>[
-        Container(
-          height: 20,
-          child: FittedBox(                                                     // forces it's child into the available space, even shrinks the text !
-            child: Text('\$${spendingAmount.toStringAsFixed(0)}')                  // to round off the decimals
-          ),
-        ),                                                
-        SizedBox(height: 4),
-        Container(
-          height:  60,
-          width: 10,
-          child: Stack(
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.grey,
-                    width: 1.0,
-                  ),
-                  color: Color.fromRGBO(220, 220, 220, 1),
-                  borderRadius: BorderRadius.circular(10),
-                ),
+    return LayoutBuilder(
+      builder: (ctx, constraints) {
+        return Column(
+          children: <Widget>[
+            Container(
+              height: constraints.maxHeight * 0.15,
+              child: FittedBox(                      // forces it's child into the available space, even shrinks the text !
+                child: Text(
+                  '\$${spendingAmount.toStringAsFixed(0)}'      // to round off the decimals
+                ) 
               ),
-              FractionallySizedBox(
-                heightFactor: spendingPctOfTotal,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
-                    borderRadius: BorderRadius.circular(10),
+            ),
+            SizedBox(height: constraints.maxHeight * 0.05),                         // used as spacing
+            Container(
+              height: constraints.maxHeight * 0.6,
+              width: 10,
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 1.0,
+                      ),
+                      color: Color.fromRGBO(220, 220, 220, 1),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
                   ),
-                ),
+                  FractionallySizedBox(
+                    heightFactor: spendingPctOfTotal,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ),
-        SizedBox(height: 4),
-        Text(label),
-      ],
+            ),
+            SizedBox(height: constraints.maxHeight * 0.05),
+            Container(
+              height: constraints.maxHeight * 0.15,
+              child: FittedBox(
+                child: Text(label)
+              )
+            ),
+          ],
+        );
+      },
     );
   }
 }
