d3c8d8994a90386453133ea3e87ae9f20b36b9be
Jitgitbit <rowowolf@gmail.com>
2020-05-18 16:47:20 +0200

    Controlling the Device Orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import './widgets/chart.dart';
 import './widgets/transaction_list.dart';
@@ -6,7 +7,14 @@ import './models/transaction.dart';
 import './widgets/new_transaction.dart';
 // import './widgets/user_transactions.dart';
 
-void main() => runApp(MyApp());
+void main(){
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([                            //----> Forcing Portrait only !
+    DeviceOrientation.portraitUp, 
+    DeviceOrientation.portraitDown
+  ]);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
